5c7a979ace4022a2d4c35ab8e2279faf61586164
Renato M. Gonçalves <gmr.renato@gmail.com>
2022-03-29 15:40:26 -0300

    feat(cart): create initial page

---
+++ lib/pages/shopping_cart.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_design_system/fruit_design_system.dart';
+
+import '../widgets/product_shopping_bag_tile.dart';
+
+class ShoppingCartPage extends StatelessWidget {
+  const ShoppingCartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(FruitUnit.small),
+                      child: BackButton(),
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          'Sacola',
+                          style: FruitTheme().primaryTextTheme.headline5,
+                        ),
+                      ),
+                    ),
+                    const FruitBoxSpacer.xSmall(),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(
+                        FruitUnit.small,
+                        FruitUnit.small,
+                        FruitUnit.xSmall,
+                        FruitUnit.small,
+                      ),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.more_vert_rounded),
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: Scrollbar(
+                    isAlwaysShown: true,
+                    child: ListView.separated(
+                      itemCount: 4,
+                      separatorBuilder: (BuildContext context, int index) {
+                        return const ProductShoppingCartTile();
+                      },
+                      itemBuilder: (BuildContext context, int index) {
+                        return const Divider();
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Material(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(FruitUnit.large),
+              ),
+            ),
+            color: FruitTheme().colorScheme.secondary,
+            child: SafeArea(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(FruitUnit.medium),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Total: R\$ 20,00',
+                          style: FruitTheme().secondaryTextTheme.bodyText1,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(
+                            FruitUnit.medium,
+                            0,
+                            FruitUnit.medium,
+                            0,
+                          ),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('Finalizar compra (checkout)'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/product_shopping_bag_tile.dart
@@ -0,0 +1,66 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:fruit_design_system/fruit_design_system.dart';
+
+class ProductShoppingCartTile extends StatelessWidget {
+  const ProductShoppingCartTile({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {},
+      child: Row(
+        children: <Widget>[
+          SizedBox(
+            height: 80,
+            width: 80,
+            child: CachedNetworkImage(
+              imageUrl:
+                  'https://www.eatme.eu/media/j4bbpwqe/aardbei.png?anchor=center&mode=crop&width=600&height=600&rnd=132629674611530000',
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  children: [
+                    const FruitBoxSpacer.xSmall(),
+                    Text(
+                      'Morango',
+                      style: FruitTheme().primaryTextTheme.bodyText1,
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const FruitBoxSpacer.xSmall(),
+                    Text(
+                      '4kg - R\$ 40,00',
+                      style: FruitTheme().primaryTextTheme.bodyText2,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Material(
+            color: FruitTheme().colorScheme.primary,
+            borderRadius: BorderRadius.circular(FruitUnit.xxxLarge),
+            child: Padding(
+              padding: const EdgeInsets.all(FruitUnit.small),
+              child: Icon(
+                Icons.remove,
+                color: FruitTheme().colorScheme.onPrimary,
+              ),
+            ),
+          ),
+          const FruitBoxSpacer(),
+        ],
+      ),
+    );
+  }
+}
