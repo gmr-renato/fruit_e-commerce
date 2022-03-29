e77ec5c29c52516898211d0daee2f007ceee3ad9
Renato M. Gonçalves <gmr.renato@gmail.com>
2022-03-29 12:28:17 -0300

    feat(product): add product page & entry points on home

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,40 +1,73 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:fruit_design_system/fruit_design_system.dart';
+import 'product_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final _bottomSheetHight = (MediaQuery.of(context).size.height -
+            MediaQuery.of(context).padding.top) /
+        MediaQuery.of(context).size.height;
     return Scaffold(
       extendBodyBehindAppBar: true,
       drawer: const Drawer(),
       appBar: AppBar(
-        title: const Text(
-          'Fruit e-commerce',
-        ),
+        title: const Text('Fruits'),
         elevation: 0,
-        backgroundColor: Colors.transparent,
         actions: [
           IconButton(
             onPressed: () {},
             icon: const Icon(Icons.search),
-          )
+          ),
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Checkout our freshest fruits',
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(FruitUnit.medium),
+            child: Text(
+              'Aproveite nossas novidades!',
+              style: FruitTheme().themeData.textTheme.headline5,
             ),
-          ],
-        ),
+          ),
+          SizedBox(
+            height: 200,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              separatorBuilder: (BuildContext context, int index) {
+                return const FruitCard();
+              },
+              itemBuilder: (BuildContext context, int index) {
+                return const FruitBoxSpacer();
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(FruitUnit.medium),
+            child: Text(
+              'Todas as frutas',
+              style: FruitTheme().themeData.textTheme.headline5,
+            ),
+          ),
+          ListView.separated(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: 10,
+            itemBuilder: (BuildContext context, int index) {
+              return FruitListTile(bottomSheetHight: _bottomSheetHight);
+            },
+            separatorBuilder: (context, index) {
+              return const FruitBoxSpacer();
+            },
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {},
-        tooltip: 'Increment',
         label: Row(
           children: [
             const Text('Sacola'),
@@ -47,6 +80,151 @@ class HomePage extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class FruitCard extends StatelessWidget {
+  const FruitCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      borderRadius: BorderRadius.circular(FruitUnit.large),
+      color: const Color(0xFFFBD1C7),
+      child: SizedBox(
+        width: 150,
+        child: Padding(
+          padding: const EdgeInsets.all(FruitUnit.medium),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Text(
+                    'Morango',
+                    style: FruitTheme().primaryTextTheme.bodyText1,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text(
+                    'R\$ 10/kg',
+                    style: FruitTheme().primaryTextTheme.bodyText2,
+                  ),
+                ],
+              ),
+              Expanded(
+                child: CachedNetworkImage(
+                  imageUrl:
+                      'https://www.eatme.eu/media/j4bbpwqe/aardbei.png?anchor=center&mode=crop&width=600&height=600&rnd=132629674611530000',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FruitListTile extends StatelessWidget {
+  const FruitListTile({
+    Key? key,
+    required this.bottomSheetHight,
+  }) : super(key: key);
+
+  final double bottomSheetHight;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        showModalBottomSheet(
+          isScrollControlled: true,
+          context: context,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(FruitUnit.large),
+            ),
+          ),
+          builder: (context) {
+            return FractionallySizedBox(
+              heightFactor: bottomSheetHight,
+              child: const ProductPage(),
+            );
+          },
+        );
+      },
+      child: Row(
+        children: <Widget>[
+          SizedBox(
+            height: 80,
+            width: 80,
+            child: CachedNetworkImage(
+              imageUrl:
+                  'https://www.eatme.eu/media/j4bbpwqe/aardbei.png?anchor=center&mode=crop&width=600&height=600&rnd=132629674611530000',
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  children: [
+                    const FruitBoxSpacer.xSmall(),
+                    Text(
+                      'Morango',
+                      style: FruitTheme().primaryTextTheme.bodyText1,
+                    ),
+                  ],
+                ),
+                const FruitBoxSpacer.xxSmall(),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.star_rounded,
+                      color: Colors.orange,
+                    ),
+                    const FruitBoxSpacer.xSmall(),
+                    Text(
+                      '4.85',
+                      style: FruitTheme().primaryTextTheme.bodyText2!.copyWith(
+                            color: Colors.orange,
+                            fontSize: 16,
+                          ),
+                    ),
+                  ],
+                ),
+                const FruitBoxSpacer.xxSmall(),
+                Row(
+                  children: [
+                    const FruitBoxSpacer.xSmall(),
+                    Text(
+                      'R\$ 10/kg',
+                      style: FruitTheme().primaryTextTheme.bodyText2,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Material(
+            color: FruitTheme().colorScheme.primary,
+            borderRadius: const BorderRadius.horizontal(
+              left: Radius.circular(FruitUnit.xxxLarge),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(FruitUnit.medium),
+              child: Icon(
+                Icons.arrow_forward_ios_rounded,
+                color: FruitTheme().colorScheme.onPrimary,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
---
+++ lib/product_page.dart
@@ -0,0 +1,163 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:fruit_design_system/fruit_design_system.dart';
+
+class ProductPage extends StatelessWidget {
+  const ProductPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(FruitUnit.small),
+                      child: CloseButton(),
+                    ),
+                    Expanded(
+                      child: SizedBox(
+                        height: 200,
+                        width: 200,
+                        child: CachedNetworkImage(
+                          imageUrl:
+                              'https://www.eatme.eu/media/j4bbpwqe/aardbei.png?anchor=center&mode=crop&width=600&height=600&rnd=132629674611530000',
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(
+                        FruitUnit.small,
+                        FruitUnit.small,
+                        FruitUnit.xSmall,
+                        FruitUnit.small,
+                      ),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.more_vert_rounded),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(FruitUnit.medium),
+                  child: Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                    style: FruitTheme().primaryTextTheme.bodyText2,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Material(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(FruitUnit.large),
+              ),
+            ),
+            color: FruitTheme().colorScheme.secondary,
+            child: SafeArea(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(FruitUnit.medium),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Morango',
+                          style: FruitTheme().secondaryTextTheme.headline6,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const FruitBoxSpacer.large(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ClipOval(
+                        child: Material(
+                          color: FruitTheme().colorScheme.primary,
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.remove,
+                              color: FruitTheme().colorScheme.onPrimary,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const FruitBoxSpacer.large(),
+                      Material(
+                        color: FruitTheme().colorScheme.primary,
+                        borderRadius: BorderRadius.circular(FruitUnit.small),
+                        child: Padding(
+                          padding: const EdgeInsets.all(FruitUnit.medium),
+                          child: Text(
+                            '500 g',
+                            style: FruitTheme().secondaryTextTheme.bodyText1,
+                          ),
+                        ),
+                      ),
+                      const FruitBoxSpacer.large(),
+                      ClipOval(
+                        child: Material(
+                          color: FruitTheme().colorScheme.primary,
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.add,
+                              color: FruitTheme().colorScheme.onPrimary,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const FruitBoxSpacer.large(),
+                  Padding(
+                    padding: const EdgeInsets.all(FruitUnit.medium),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Total: R\$ 20,00',
+                          style: FruitTheme().secondaryTextTheme.headline6,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const FruitBoxSpacer(),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(
+                            FruitUnit.medium,
+                            FruitUnit.medium,
+                            FruitUnit.medium,
+                            0,
+                          ),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('Adicionar à sacola'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
