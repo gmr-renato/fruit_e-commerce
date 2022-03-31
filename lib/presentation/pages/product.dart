09a02bd23362a2c47bb3655957b6d9524be77200
Renato M. Gonçalves <gmr.renato@gmail.com>
2022-03-31 19:48:33 -0300

    refactor(product): isolate statefull part of product page

--- lib/presentation/pages/product.dart
+++ lib/presentation/pages/product.dart
@@ -80,116 +80,140 @@ class ProductPage extends StatelessWidget {
                     ),
                   ),
                 ),
+                _QuantityAndValue(product),
               ],
             ),
           ),
-          Material(
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(FruitUnit.large),
-              ),
-            ),
-            color: getIt<FruitTheme>().colorScheme.secondary,
-            child: SafeArea(
-              child: Column(
+        ],
+      ),
+    );
+  }
+}
+
+class _QuantityAndValue extends StatefulWidget {
+  const _QuantityAndValue(this.product, {Key? key}) : super(key: key);
+
+  final Product product;
+
+  @override
+  State<_QuantityAndValue> createState() => _QuantityAndValueState();
+}
+
+class _QuantityAndValueState extends State<_QuantityAndValue> {
+  int _quantity = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(FruitUnit.large),
+        ),
+      ),
+      color: getIt<FruitTheme>().colorScheme.secondary,
+      child: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(FruitUnit.medium),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(FruitUnit.medium),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          product.i18nDetails[IsoCountryCode('BR')]!.name
-                              .getOrCrash(),
-                          style:
-                              getIt<FruitTheme>().secondaryTextTheme.headline6,
-                        ),
-                      ],
-                    ),
-                  ),
-                  const FruitBoxSpacer.large(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ClipOval(
-                        child: Material(
-                          color: getIt<FruitTheme>().colorScheme.primary,
-                          child: IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.remove,
-                              color: getIt<FruitTheme>().colorScheme.onPrimary,
-                            ),
-                          ),
-                        ),
-                      ),
-                      const FruitBoxSpacer.large(),
-                      Material(
-                        color: getIt<FruitTheme>().colorScheme.primary,
-                        borderRadius: BorderRadius.circular(FruitUnit.small),
-                        child: Padding(
-                          padding: const EdgeInsets.all(FruitUnit.medium),
-                          child: Text(
-                            '${product.i18nDetails[IsoCountryCode('BR')]!.interval.getOrCrash()} ${product.i18nDetails[IsoCountryCode('BR')]!.unit.getOrCrash()}',
-                            style: getIt<FruitTheme>()
-                                .secondaryTextTheme
-                                .bodyText1,
-                          ),
-                        ),
-                      ),
-                      const FruitBoxSpacer.large(),
-                      ClipOval(
-                        child: Material(
-                          color: getIt<FruitTheme>().colorScheme.primary,
-                          child: IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.add,
-                              color: getIt<FruitTheme>().colorScheme.onPrimary,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const FruitBoxSpacer.large(),
-                  Padding(
-                    padding: const EdgeInsets.all(FruitUnit.medium),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Total: R\$ ${product.i18nDetails[IsoCountryCode('BR')]!.price.getOrCrash()}',
-                          style:
-                              getIt<FruitTheme>().secondaryTextTheme.headline6,
-                        ),
-                      ],
-                    ),
-                  ),
-                  const FruitBoxSpacer(),
-                  Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.fromLTRB(
-                            FruitUnit.medium,
-                            FruitUnit.small,
-                            FruitUnit.medium,
-                            0,
-                          ),
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            child: const Text('Adicionar à sacola'),
-                          ),
-                        ),
-                      ),
-                    ],
+                  Text(
+                    widget.product.i18nDetails[IsoCountryCode('BR')]!.name
+                        .getOrCrash(),
+                    style: getIt<FruitTheme>().secondaryTextTheme.headline6,
                   ),
                 ],
               ),
             ),
-          )
-        ],
+            const FruitBoxSpacer.large(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ClipOval(
+                  child: Material(
+                    color: getIt<FruitTheme>().colorScheme.primary,
+                    child: IconButton(
+                      onPressed: () {
+                        if (_quantity > 1) {
+                          _quantity--;
+                          setState(() {});
+                        }
+                      },
+                      icon: Icon(
+                        Icons.remove,
+                        color: getIt<FruitTheme>().colorScheme.onPrimary,
+                      ),
+                    ),
+                  ),
+                ),
+                const FruitBoxSpacer.large(),
+                Material(
+                  color: getIt<FruitTheme>().colorScheme.primary,
+                  borderRadius: BorderRadius.circular(FruitUnit.small),
+                  child: Padding(
+                    padding: const EdgeInsets.all(FruitUnit.medium),
+                    child: Text(
+                      '${(widget.product.i18nDetails[IsoCountryCode('BR')]!.interval.getOrCrash() * _quantity).roundToDouble()}  ${widget.product.i18nDetails[IsoCountryCode('BR')]!.unit.getOrCrash()}',
+                      style: getIt<FruitTheme>().secondaryTextTheme.bodyText1,
+                    ),
+                  ),
+                ),
+                const FruitBoxSpacer.large(),
+                ClipOval(
+                  child: Material(
+                    color: getIt<FruitTheme>().colorScheme.primary,
+                    child: IconButton(
+                      onPressed: () {
+                        if (_quantity < 20) {
+                          _quantity++;
+                          setState(() {});
+                        }
+                      },
+                      icon: Icon(
+                        Icons.add,
+                        color: getIt<FruitTheme>().colorScheme.onPrimary,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const FruitBoxSpacer.large(),
+            Padding(
+              padding: const EdgeInsets.all(FruitUnit.medium),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Total: R\$ ${widget.product.i18nDetails[IsoCountryCode('BR')]!.price.getOrCrash() * _quantity}',
+                    style: getIt<FruitTheme>().secondaryTextTheme.headline6,
+                  ),
+                ],
+              ),
+            ),
+            const FruitBoxSpacer(),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(
+                      FruitUnit.medium,
+                      FruitUnit.small,
+                      FruitUnit.medium,
+                      0,
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('Adicionar à sacola'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
