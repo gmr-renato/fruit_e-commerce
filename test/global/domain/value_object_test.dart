c332c9d68068c7afa37736aa5e3d5100aaeb99c0
Renato M. Gonçalves <gmr.renato@gmail.com>
2022-04-21 09:45:50 -0300

    test(global): add value object tests description

---
+++ test/global/domain/value_object_test.dart
@@ -0,0 +1,8 @@
+
+/// Force primitive typing
+/// Force implementation of getter get value
+/// 
+/// Use implementation of failureOrUnit
+/// Use implementation of isValid
+/// Use implementation of isInvalid
+/// Use implementation of getOrCrash
