5f8a40adc2381d34380f257fd9e87b45f4465c34
Renato M. Gonçalves <gmr.renato@gmail.com>
2022-04-21 01:18:42 -0300

    test(marketplace): add product_page_tests description
    This describes the planned tests for the product page

---
+++ test/marketplace/presentation/product_page_tests.dart
@@ -0,0 +1,45 @@
+
+
+/// Tests:
+/// LAYOUT STRUCTURE
+/// Page should have a button to close the page
+/// Page should have a image containing the product image
+/// Page should have a description containing the product description
+/// 
+/// Page should have a title containing the product name
+/// Page should have a text to display the quantity
+/// Page should have an add icon to increase the quantity of the product
+/// Page should have a remove icon to decrease the quantity of the product
+/// Page should have a text containing the total price
+/// Page should have a button to add the product to the cart
+/// 
+/// DESIGN & APPEREANCE:
+/// The background of the image should be surface color
+/// 
+/// The foreground of the description should be onSurface color
+/// The style of the description should match body text style from fruit theme
+/// 
+/// The background of the title should be secondary color
+/// The style of the title should match medium header style from fruit theme
+/// 
+/// The background of the quantity should be primary color
+/// The style of the quantity should match medium header style from fruit theme
+/// 
+/// The background of the increment icon should be primary color
+/// The color of the increment icon should be onPrimary color
+/// 
+/// The background of the decrement button should be primary color
+/// The color of the decrement icon should be onPrimary color
+/// 
+/// The background of the total price should be secondary color
+/// The add to cart button should have the primary button colors
+/// 
+/// BEHAVIOUR:
+/// The back button should close the current page
+/// Dragging in the description should show more text
+/// The add quantity button should increment the quantity
+/// The remove quantity button should decrement the quantity
+/// The total price should update as the quantity changes 
+/// The quantity should be limited to min 1
+/// The quantity should be limited to max 20
+/// The add to cart button should add the product to the cart, close the current page and open the cart page
