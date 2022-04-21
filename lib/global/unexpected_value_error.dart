b11d31be93e25854ec0bca24c7a40bcf098c5135
Renato M. Gonçalves <gmr.renato@gmail.com>
2022-04-21 09:01:14 -0300

    feat(global): add base class for failure & error

---
+++ lib/global/unexpected_value_error.dart
@@ -0,0 +1,14 @@
+import 'value_failure.dart';
+
+class UnexpectedValueError extends Error {
+  UnexpectedValueError(this.valueFailure);
+
+  final ValueFailure valueFailure;
+
+  @override
+  String toString() {
+    const explanation =
+        'Encountered a ValueFailure at an unrecoverable point. Terminating.';
+    return Error.safeToString('$explanation value failure was: $valueFailure');
+  }
+}
