476135365e78084e416c68fe7a91ea3e169e0696
Renato M. Gonçalves <gmr.renato@gmail.com>
2022-04-25 20:01:18 -0300

    test(global): [F] scopes for validateStringNotEmpty

--- test/global/domain/value_validators_test.dart
+++ test/global/domain/value_validators_test.dart
@@ -1,12 +1,36 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fruit_ecommerce/global/domain/value_failure.dart';
+import 'package:fruit_ecommerce/global/domain/value_validators.dart';
+
+import '../../setup/test_inputs.dart';
 
 void main() {
   group(
     'validateStringNotEmpty',
     () {
-      // validateStringNotEmpty()
-      // Should return a right(ValueFailure.empty) if the input is empty.
-      // Should return left(input) if the input is not empty.
+      test(
+        'Should return a left(ValueFailure.empty) if the input is empty',
+        () {
+          const input = TestInputs.emptyString;
+          const expected = ValueFailure.empty(failedValue: '');
+
+          validateStringNotEmpty(input).fold(
+            (l) => expect(l, expected),
+            (r) => throwRightSide,
+          );
+        },
+      );
+      test(
+        'Should return a right(input) if the input is not empty',
+        () {
+          const input = TestInputs.singleWord;
+
+          validateStringNotEmpty(input).fold(
+            (l) => throwLeftSide,
+            (r) => expect(r, TestInputs.singleWord),
+          );
+        },
+      );
     },
   );
 }
