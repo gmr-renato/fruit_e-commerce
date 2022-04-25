973e0a11d8ce6ce3df6918bad0caf0f7993a56d1
Renato M. Gonçalves <gmr.renato@gmail.com>
2022-04-25 20:58:17 -0300

    test(global): add test description for UniqueID

---
+++ test/global/domain/value_objects_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group(
+    'UniqueID -',
+    () {
+      test(
+        'Should be valid on .generate() constructor',
+        () {},
+      );
+      test(
+        'Should match input from right() on valid UID input',
+        () {},
+      );
+      test(
+        'Should match ValueFailure.empty from left() on empty input',
+        () {},
+      );
+    },
+  );
+}
