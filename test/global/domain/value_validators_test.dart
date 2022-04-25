ff8a2a93bd9528931a3ea58a887130dd35e1ba58
Renato M. Gonçalves <gmr.renato@gmail.com>
2022-04-25 19:50:59 -0300

    test(global): plan test for validateStringNotEmpty

---
+++ test/global/domain/value_validators_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group(
+    'validateStringNotEmpty',
+    () {
+      // validateStringNotEmpty()
+      // Should return a right(ValueFailure.empty) if the input is empty.
+      // Should return left(input) if the input is not empty.
+    },
+  );
+}
