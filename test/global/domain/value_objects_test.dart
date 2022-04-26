a3b55be523a6b02b74bc73d3e6bd5aade064ff65
Renato M. Gonçalves <gmr.renato@gmail.com>
2022-04-25 22:35:53 -0300

    test(global): :white_check_mark: add inherited methods for UniqueID

--- test/global/domain/value_objects_test.dart
+++ test/global/domain/value_objects_test.dart
@@ -1,3 +1,4 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fruit_ecommerce/global/domain/value_failure.dart';
 import 'package:fruit_ecommerce/global/domain/value_objects.dart';
@@ -9,6 +10,9 @@ void main() {
   group(
     'UniqueID -',
     () {
+      const validInput = TestInputs.validUID;
+      const invalidInput = TestInputs.emptyString;
+
       late UniqueID uid;
 
       test(
@@ -16,28 +20,27 @@ void main() {
         () {
           uid = UniqueID.generate();
           expect(uid.isValid(), true);
+          expect(uid.isInvalid(), false);
         },
       );
       test(
         'Should be valid & match input from right() on valid UID input',
         () {
-          const input = TestInputs.validUID;
-          uid = UniqueID.fromSafeString(input);
+          uid = UniqueID.fromSafeString(validInput);
 
           expect(uid.isValid(), true);
           expect(uid.isInvalid(), false);
 
           uid.value.fold(
             (l) => throwLeftSide(),
-            (r) => expect(r, input),
+            (r) => expect(r, validInput),
           );
         },
       );
       test(
         'Should be invalid & match ValueFailure.empty from left() on empty input',
         () {
-          const input = TestInputs.emptyString;
-          uid = UniqueID.fromSafeString(input);
+          uid = UniqueID.fromSafeString(invalidInput);
           const expectedFailure = ValueFailure.empty(failedValue: '');
 
           expect(uid.isInvalid(), true);
@@ -46,6 +49,48 @@ void main() {
           uid.value.fold(
             (l) => expect(l, expectedFailure),
             (r) => throwRightSide(),
+          );
+        },
+      );
+      test(
+        'Validate inherited failureOrUnit()',
+        () {
+          uid = UniqueID.fromSafeString(invalidInput);
+
+          uid.failureOrUnit.fold(
+            (l) => expect(l, isA<ValueFailure>()),
+            (r) => throwRightSide(),
+          );
+
+          uid = UniqueID.fromSafeString(validInput);
+
+          uid.failureOrUnit.fold(
+            (l) => throwLeftSide(),
+            (r) => expect(r, isA<Unit>()),
+          );
+        },
+      );
+      test(
+        'Validate inherited equality operator',
+        () {
+          final uid1 = UniqueID.fromSafeString(validInput);
+          final uid2 = UniqueID.fromSafeString(validInput);
+
+          expect(uid1 == uid2, true);
+        },
+      );
+      test(
+        'Validate inherited toString()',
+        () {
+          final validUid = UniqueID.fromSafeString(validInput);
+
+          expect(validUid.toString(), validInput);
+
+          final invalidUid = UniqueID.fromSafeString(invalidInput);
+
+          expect(
+            invalidUid.toString(),
+            const ValueFailure.empty(failedValue: '').toString(),
           );
         },
       );
